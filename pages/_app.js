7a54e8a980674fe5d9b7c5f88d7183f333bddc0d
Eugen <kovalchuk.eak@gmail.com>
2022-11-14 14:43:44 +0200

    start

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import '../styles/globals.css'
 
 function Registration({ Component, pageProps }) {
-  return <Component { ...pageProps } />
+  return <ChakraProvider><Component { ...pageProps } /></ChakraProvider>
 }
 
 export default Registration
